002a6e9b1f08db122579f3bc173076894b773e40
alioo20051 <elouahhabiali@gmail.com>
2023-03-05 20:50:52 +0100

    end

---
+++ TextChangerScripts.js
@@ -0,0 +1,27 @@
+//Get the place that you want to change Frome the HTMLDoc
+const chengingText = document.querySelector(//put The element here);
+//Arry of words
+const arrOfWords = ["Enjoy","And Discover Anthor World","and live Happy"];
+//index number for the arry of words
+var indexN = -1;
+//the indexN update Function  
+function indexNUpdate() {
+indexN++
+if(indexN == 3){indexN = 0};
+changeTextFunction();
+return setTimeout(indexNUpdate,1500);
+};
+//update the words function
+function changeTextFunction(){
+chengingText.textContent = arrOfWords[indexN];
+};
+//the strat of all functions
+setTimeout(indexNUpdate,2000);
+/* 
+   Note:
+   if you want to make the words change faster or slower 
+   just change the time in setTimeout functions.
+   Happy coding Brothers and sisters
+*/
+
+//Developet and created By AliElouahhabi
